7ded544d1c5f1acc9cf15aa8cc1f6435273e0d30
Saakshi Raut <saakshiraut28@gmail.com>
2024-08-21 22:45:35 +0530

    modify: main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,17 @@
 /** @format */
 
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 import { Buffer } from "buffer";
 
 if (!window.Buffer) {
   window.Buffer = Buffer;
 }
 
-// Your existing imports and code
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-
-ReactDOM.render(<App />, document.getElementById("root"));
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
